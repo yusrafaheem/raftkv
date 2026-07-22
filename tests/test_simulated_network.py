ee73ded6aa91f0ef1384c7b5dcd04e594d9b054d
yusrafaheem <yusra.faheem@gwmail.gwu.edu>
2026-07-21 20:29:49 -0400

    Add test_simulated_network.py: direct tests of delay, drop_between, partition, and drop_all_pending

---
+++ tests/test_simulated_network.py
@@ -0,0 +1,70 @@
+"""Direct unit tests of `SimulatedNetwork`, isolated from any `RaftNode`
+or `SimulatedCluster` -- these just check the delivery/fault-injection
+mechanics themselves against plain `Message` objects."""
+
+import os
+import random
+import sys
+import unittest
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src"))
+
+from raftkv.raft.rpc import Message, RequestVoteReply
+from raftkv.transport.simulated import SimulatedNetwork
+
+
+def msg(src, dst):
+    return Message(src, dst, RequestVoteReply(term=1, vote_granted=True, voter_id=src))
+
+
+class TestBasicDelivery(unittest.TestCase):
+    def test_a_message_is_delivered_within_its_delay_range(self):
+        net = SimulatedNetwork(random.Random(0), delay_range=(2, 2))
+        net.send(msg(1, 2))
+        self.assertEqual(net.advance(), [])  # tick 1: not due yet
+        self.assertEqual(net.advance(), [msg(1, 2)])  # tick 2: due now
+
+    def test_messages_sent_counter_increments_even_for_dropped_messages(self):
+        net = SimulatedNetwork(random.Random(0))
+        net.drop_between(1, 2)
+        net.send(msg(1, 2))
+        self.assertEqual(net.messages_sent, 1)
+        self.assertEqual(net.messages_dropped, 1)
+
+
+class TestDropBetween(unittest.TestCase):
+    def test_dropped_pair_blocks_messages_in_either_direction(self):
+        net = SimulatedNetwork(random.Random(0))
+        net.drop_between(1, 2)
+        net.send(msg(1, 2))
+        net.send(msg(2, 1))
+        self.assertEqual(net.pending_count(), 0)
+
+    def test_restore_between_undoes_a_drop(self):
+        net = SimulatedNetwork(random.Random(0))
+        net.drop_between(1, 2)
+        net.restore_between(1, 2)
+        net.send(msg(1, 2))
+        self.assertEqual(net.pending_count(), 1)
+
+
+class TestPartitionWithAnUnlistedNode(unittest.TestCase):
+    def test_a_node_missing_from_every_partition_group_is_treated_as_isolated(self):
+        net = SimulatedNetwork(random.Random(0))
+        net.partition([{1, 2}])  # node 3 is in no group at all
+        net.send(msg(1, 3))
+        self.assertEqual(net.pending_count(), 0)
+
+
+class TestDropAllPending(unittest.TestCase):
+    def test_drop_all_pending_clears_the_queue_and_counts_as_dropped(self):
+        net = SimulatedNetwork(random.Random(0))
+        net.send(msg(1, 2))
+        net.send(msg(1, 3))
+        net.drop_all_pending()
+        self.assertEqual(net.pending_count(), 0)
+        self.assertEqual(net.messages_dropped, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
